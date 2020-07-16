f193e979f33d18a89d0c544267e9e6f1418997a3
NateOwens <nathanieltylerowens@gmail.com>
2020-07-16 18:48:52 -0500

    added to get webpack working

--- src/javascripts/components/visitor/visitor.js
+++ src/javascripts/components/visitor/visitor.js
@@ -5,16 +5,31 @@ import utils from '../../helpers';
 const printVisitor = () => {
   visitorData.getVisitorData()
     .then((visitor) =>{
-      let domstring =`
+      let domString =`
       <div id="visitorDiv">
-        <div class="card" style="width: 18rem;">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
+        <h2 class="text-center">Visitors<h2>
+        <div class="d-flex flex-wrap">`
+
+    visitors.forEach((visitor) => {
+      domString +=`
+          <div id="${visitor.id}" class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">${visitor.name}</h5>
+              <p class="gender">${visitor.gender}</p>
+              <p class="attend">${visitor.attendance}</p>
+              <a href="#" class="btn btn-primary" id="add-visitor"><i class="fas fa-plus"></i></a>
+              <a href="#" class="btn btn-primary" id="delete-visitor"><i class="fas fa-trash"></i></a>
+            </div>
+          </div>`
+
+    domString +=`
         </div>
       </div>
-       `
+       `;
+    });
+    utils.printToDom('#content', domString);
     })
+    .catch((err) => console.error('visitors broke', err));
 };
+
+export default { printVisitor };
--- src/javascripts/helpers/listeners.js
+++ src/javascripts/helpers/listeners.js
@@ -1,9 +1,11 @@
 import buildVendors from '../components/vendor/vendorList';
 import displayRides from '../components/displayRides/displayRides';
+import displayVisitors from '../components/visitor/visitor';
 
 const createListeners = () => {
   $('body').on('click', '#navbar-vendors', buildVendors.buildVendorList);
   $('body').on('click', '.rideLink', displayRides.buildRideModule);
+  $('body').on('click', '.visitorLink', displayVisitors.printVisitor);
 };
 
 export default {

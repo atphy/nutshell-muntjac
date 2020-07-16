b49ef5b5db945406d74d09a269393ce08ecdddc9
Nick Walters <nswalters.s@gmail.com>
2020-07-15 21:37:47 -0500

    Add delete trash icon in card header

--- src/javascripts/components/vendor/vendor.js
+++ src/javascripts/components/vendor/vendor.js
@@ -2,7 +2,10 @@ const vendorCardMaker = (vendor) => {
   const domString = `
   <div class="col-3">
     <div class="card border-0 rounded-0 bg-light text-dark mb-3" id=${vendor.id}>
-      <div class="card-header text-center">${vendor.name}</div>
+      <div class="card-header text-center d-flex justify-content-between">
+        <span class="m-auto">${vendor.name}</span>
+        <button class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
+      </div>
       <div class="card-body">
         <h5 class="card-title">${vendor.product}</h5>
         <p class="card-text">${vendor.address}</p>

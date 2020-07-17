fcbbe3cc77b6376b9e5ba87a2d7518fdd45d7750
Nick Walters <nswalters.s@gmail.com>
2020-07-17 17:24:33 -0500

    Added specific class to attach delete button listener to

--- src/javascripts/components/vendor/vendor.js
+++ src/javascripts/components/vendor/vendor.js
@@ -4,7 +4,7 @@ const vendorCardMaker = (vendor) => {
     <div class="card border-0 rounded-0 bg-light text-dark mb-3" id=${vendor.id}>
       <div class="card-header text-center d-flex justify-content-between">
         <span class="m-auto">${vendor.name}</span>
-        <button class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
+        <button class="btn btn-danger delete-vendor"><i class="fas fa-trash-alt"></i></button>
       </div>
       <div class="card-body">
         <h5 class="card-title">${vendor.product}</h5>

40adf3522d0c4646f3f3147ebdef9ad8a9b60305
Nick Walters <nswalters.s@gmail.com>
2020-07-17 17:23:14 -0500

    Add click event and handler function for delete vendor

--- src/javascripts/helpers/listeners.js
+++ src/javascripts/helpers/listeners.js
@@ -1,9 +1,20 @@
 import buildVendors from '../components/vendor/vendorList';
 import displayRides from '../components/displayRides/displayRides';
+import vendorData from './data/vendor/vendorData';
+
+const deleteVendorEvent = (e) => {
+  const vendorId = e.target.closest('.card').id;
+  vendorData.deleteVendor(vendorId)
+    .then(() => {
+      buildVendors.buildVendorList();
+    })
+    .catch((err) => console.error('Could not delete vendor', err));
+};
 
 const createListeners = () => {
   $('body').on('click', '#navbar-vendors', buildVendors.buildVendorList);
   $('body').on('click', '.rideLink', displayRides.buildRideModule);
+  $('body').on('click', '.delete-vendor', deleteVendorEvent);
 };
 
 export default {

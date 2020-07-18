b67a04cf12bf6ff38f7d093231cab25ad31c3300
Nick Walters <nswalters.s@gmail.com>
2020-07-18 13:13:48 -0500

    Fix typos

--- src/javascripts/components/ride/homescreen/homescreen.js
+++ src/javascripts/components/ride/homescreen/homescreen.js
@@ -8,7 +8,7 @@ const buildHomeScreen = () => {
   <img src="https://i.imgur.com/ZkYEPXi.png"></img>
   <h3 class="homeH3">A Whale Of A Good Time!</h3>
   <div class="homeMessage">
-  <p class="homeP">Thanks for visiting the new admin panel for Whalan Park, built buy Nutshell Muntjac. From the Admin Panel you can view and edit park visitors, vendors, staff and rides. Log in to access CRUD features.</p>
+  <p class="homeP">Thanks for visiting the new admin panel for Whalom Park, built by Nutshell Muntjac. From the Admin Panel you can view and edit park visitors, vendors, staff and rides. Log in to access CRUD features.</p>
   </div>
   </div>`;
   utils.printToDom('#content', domString);

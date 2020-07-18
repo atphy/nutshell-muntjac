266231f83b8c5faee2701f39199a727555a6379c
Matt Logan <mlogan5212@gmail.com>
2020-07-17 21:10:42 -0500

    add alert for broken down rides. style cards

---
+++ src/javascripts/components/ride/updateRide/updateRide.js
@@ -0,0 +1,10 @@
+import utils from '../../../helpers/utils';
+
+const brokenRideAlert = () => {
+  const domString = 'Hello';
+  utils.printToDom('.rideAlert', domString);
+};
+
+export default {
+  brokenRideAlert,
+};

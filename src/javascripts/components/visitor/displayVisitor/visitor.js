3430e14ba6866ccd5ad02727c0eeb095561b1761
NateOwens <nathanieltylerowens@gmail.com>
2020-07-18 10:42:25 -0500

    added styling to form

--- src/javascripts/components/visitor/displayVisitor/visitor.js
+++ src/javascripts/components/visitor/displayVisitor/visitor.js
@@ -11,7 +11,7 @@ const printVisitor = () => {
           <div id="new-vis">
             <button class="btn btn-primary" id="add-vis-form"><i class="fas fa-plus"></i> NEW VISITOR</button>
           </div>
-          <div id="new-vis-form"><div>
+          <div id="new-vis-form"></div>
         <div class="d-flex flex-wrap">`;
 
       visitors.forEach((visitor) => {
@@ -31,6 +31,7 @@ const printVisitor = () => {
       </div>
        `;
       utils.printToDom('#content', domString);
+      utils.printToDom('#new-vis-form', '');
     })
     .catch((err) => console.error('visitors broke', err));
 };

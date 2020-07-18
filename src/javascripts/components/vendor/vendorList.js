eca28197b47ee9a35c354fa30470ef3a3836ca68
Nick Walters <nswalters.s@gmail.com>
2020-07-17 19:59:22 -0500

    Add modal for handling alert when user performs an action that requires authentication

--- src/javascripts/components/vendor/vendorList.js
+++ src/javascripts/components/vendor/vendorList.js
@@ -6,6 +6,24 @@ const buildVendorList = () => {
   vendorData.getVendors()
     .then((vendors) => {
       let domString = `
+      <div class="modal" tabindex="-1" role="dialog" id="myModal">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Authentication Required</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>You must be logged in to perform that action.</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
       <h2 class="text-center">Whalom Park Vendors</h2>
       <div class="d-flex flex-wrap">
       `;

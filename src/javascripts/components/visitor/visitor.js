ce2215e8378add31b36a5dddd0effff95ffa5060
Joshua Medlen <joshua246@gmail.com>
2020-08-01 10:49:49 -0500

    fixed add button issue with not appearing

--- src/javascripts/components/visitor/visitor.js
+++ src/javascripts/components/visitor/visitor.js
@@ -1,5 +1,6 @@
 import './visitor.scss';
 import utils from '../../helpers/utils';
+import authData from '../../helpers/data/authData';
 
 const getVisitors = () => utils.readData('Visitor');
 const getVisitorLog = () => utils.readData('visitorLog');
@@ -11,7 +12,7 @@ const printVisitor = () => {
     .then((visitors) => {
       let domString = `
       <div id="visitor-div">
-        <h2 class="text-center" id="vis-heading">Visitors<h2>
+        <h2 class="text-center homeH3" id="vis-heading">Visitors<h2>
         <div class="text-center mb-3" id="add-button"></div>
         <button type="button" class="btn btn-success" id="buy-something"><i class="fas fa-dollar-sign"></i>Buy Something</button>
         <button type="button" class="btn btn-warning" id="visitor-log-btn"><i class="fas fa-clipboard-list"></i>Visitor Activity</button>
@@ -38,6 +39,7 @@ const printVisitor = () => {
       <div id="visitor-activity" class="hide"></div>
        `;
       utils.printToDom('#content', domString);
+      authData.checkLoginStatus();
     })
     .catch((err) => console.error('visitors broke', err));
   // eslint-disable-next-line no-use-before-define
